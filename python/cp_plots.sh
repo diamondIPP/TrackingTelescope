cf3c4ac5505aab1b3826318043c7e0c128df0bea
Gregor Kasieczka <g.kasieczka@gmail.com>
2015-04-09 10:27:52 +0200

    Small Cleanup

---
+++ python/cp_plots.sh
@@ -0,0 +1,7 @@
+export TARGET=for_paper
+cp plots/SumCharge4_ROC4_z_Telescope1_S105_up.pdf $TARGET
+cp SumCharge_Telescope1_S105_all.pdf $TARGET
+cp ClusterSize_Telescope1_S105_all.pdf $TARGET
+cp plots/SumCharge4_ROC3_z_Telescope1_S86_up.pdf $TARGET
+cp SumCharge_Telescope1_S86_all.pdf $TARGET
+cp ClusterSize_Telescope1_S86_all.pdf $TARGET
