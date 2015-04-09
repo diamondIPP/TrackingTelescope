4d063df13ce285d6c727088b53f8398c1d6857a5
Gregor Kasieczka <g.kasieczka@gmail.com>
2015-04-09 10:25:43 +0200

    Updated python plotting

--- python/ClusterSizeAndCharge.py
+++ python/ClusterSizeAndCharge.py
@@ -112,15 +112,18 @@ cluster_size = {}
 cluster_size_fraction_1 = {}
 cluster_size_fraction_12 = {}
 second_charge = {}
+cluster_size_and_charge = {}
+
 
 #print "{0:>4} {1:>4} {2:>7} {3:>6} {4:>6}".format("Run", "ROC", "Mean", "FWHM", "Entr.", "Gaus", "Left", "Right")
-print "{0:>4}   {1:>10}   {2:>4}   {3:>7} {4:>6} {5:>6} {6:>6}".format("Run", "Rate (kHz)", "I", "Mean", "RMS", "Entr.", "SE")
+print "{0:>4}   {1:>10}   {2:>4}   {3:>7} {4:>6} {5:>6} {6:>6}".format("Run", "Flux (kHz)", "I", "Mean", "RMS", "Entr.", "SE")
 
 
 means = {
     "first_three" : [],
     "low_rate"    : [],
     "high_rate"   : [],
+    "all" : []
 }
 
 
@@ -133,14 +136,24 @@ for i_run, run in enumerate(di_runs):
     cluster_size_fraction_1[run] = []
     cluster_size_fraction_12[run] = []
     second_charge[run] = []
+    cluster_size_and_charge[run] = []
 
     input_rootfile_name = "../plots/000" + str(run) + "/histos.root"
     f = ROOT.TFile.Open(input_rootfile_name)
 
     for i_roc in range(1, 5):
+
+        rs = root_style()
+        rs.set_style(1000,1000,1)
+        ROOT.gROOT.ForceStyle()
+        c = rs.get_canvas("")        
+        
+
         # Get the mean cluster size
         h_cs = f.Get("ClusterSize_ROC" + str(i_roc))
         h_cs_proj = h_cs.Project3D("z")
+        h_cs_proj.SetBinContent(1,0)
+
         cluster_size[run].append(h_cs_proj.GetMean())
 
         # Get the the fraction of events where the cluster size:
@@ -166,17 +179,57 @@ for i_run, run in enumerate(di_runs):
         htmp.SetBinContent(1, 0)
         sum_charge[run].append(htmp.GetMean())
         
+        
+        
+        combined  = ROOT.TGraphErrors()        
+        tmp_1st = h_charge.Clone()
+        tmp_2nd = h_se.Clone()
+        tmp_sum = h_sumcharge.Clone()
+        tmp_cs  = h_cs.Clone()
 
-        
-        
-        rs = root_style()
-        rs.set_style(1000,1000,1)
-        ROOT.gROOT.ForceStyle()
-        
-        c = rs.get_canvas("")
-            
-        #if run in [322, 325, 327, 330, 333]:
+        tmp_1st.GetZaxis().SetRange(2,tmp_1st.GetZaxis().GetNbins())
+        tmp_2nd.GetZaxis().SetRange(2,tmp_2nd.GetZaxis().GetNbins())
+        tmp_sum.GetZaxis().SetRange(2,tmp_sum.GetZaxis().GetNbins())
+        tmp_cs.GetZaxis().SetRange(2,tmp_cs.GetZaxis().GetNbins())
+
+        h_1st_xy = tmp_1st.Project3DProfile("xy")
+        h_2nd_xy = tmp_2nd.Project3DProfile("xy")
+        h_sum_xy = tmp_sum.Project3DProfile("xy")
+        h_cs_xy  = tmp_cs.Project3DProfile("xy")
+
         if True:
+            h_bg = ROOT.TH2D("","",50,0,3,50,0,30000)
+
+            for ibin_x in range(1, h_sum_xy.GetNbinsX()+1):
+                for ibin_y in range(1, h_sum_xy.GetNbinsY()+1):
+                    
+                    if (h_cs_xy.GetBinError(ibin_x, ibin_y) > 0 
+                        and h_sum_xy.GetBinError(ibin_x, ibin_y) > 0 
+                        #and h_sum_xy.GetBinError(ibin_x, ibin_y)/h_sum_xy.GetBinContent(ibin_x, ibin_y) < 0.1 
+                        #and 10000 < h_sum_xy.GetBinContent(ibin_x, ibin_y) < 30000
+                    ):
+
+                        n = combined.GetN()
+                        combined.SetPoint( n, h_cs_xy.GetBinContent(ibin_x, ibin_y), h_1st_xy.GetBinContent(ibin_x, ibin_y))
+
+                        #print h_1st_xy.GetBinContent(ibin_x, ibin_y),  h_cs_xy.GetBinContent(ibin_x, ibin_y)
+                        #combined.SetPointError( n, 
+                        #                        h_cs_xy.GetBinError(ibin_x, ibin_y), 
+                        #                        h_sum_xy.GetBinError(ibin_x, ibin_y))
+                        #h_bg.Fill(h_cs_xy.GetBinContent(ibin_x, ibin_y), h_sum_xy.GetBinContent(ibin_x, ibin_y))
+                        
+
+            fun = ROOT.TF1("","[0]+[1]*x",1, 3)
+            fun.SetParameter(0, 15000)
+            fun.SetParameter(1, 0)
+
+            combined.Fit(fun,"RQ")
+            
+            h_bg.Draw()
+            combined.Draw("PSAME")
+            c.Print("cs_sum_{0}_{1}.pdf".format(i_roc, run))
+                            
+        if False:
             h_sumcharge_proj_z = h_sumcharge.Project3D("z")
             if i_roc in [4]:
                 
@@ -186,12 +239,21 @@ for i_run, run in enumerate(di_runs):
                     if i_iter == 0:
                         h_sumcharge_proj_z.SetBinContent(1,0)                    
 
+                        means["all"].append(h_sumcharge_proj_z.GetMean())
 
                         if run in [322, 325, 327]:
                             means["first_three"].append(h_sumcharge_proj_z.GetMean()) 
                         if run in [322, 347, 348]:
                             means["low_rate"].append(h_sumcharge_proj_z.GetMean())
                         if run in [333, 350, 352]:
+                            means["high_rate"].append(h_sumcharge_proj_z.GetMean())
+
+
+                        if run in [466, 467, 469]:
+                            means["first_three"].append(h_sumcharge_proj_z.GetMean()) 
+                        if run in [466, 475]:
+                            means["low_rate"].append(h_sumcharge_proj_z.GetMean())
+                        if run in [470, 476, 478]:
                             means["high_rate"].append(h_sumcharge_proj_z.GetMean())
                            
 
@@ -206,39 +268,60 @@ for i_run, run in enumerate(di_runs):
                         for i_bin in range( h_sumcharge_proj_z.FindBin(7500)+1):
                             h_sumcharge_proj_z.SetBinContent(i_bin, 0)
                     
-
                     entries = h_sumcharge_proj_z.Integral()
                     mean = h_sumcharge_proj_z.GetMean()
                     rms  = h_sumcharge_proj_z.GetRMS()
-                    #fwhm = get_fwhm(h_sumcharge_proj_z)
-                    #sigma = fwhm/2.
-                    #
-                    #bin_4_sigma_left = h_sumcharge_proj_z.FindBin(mean - 4*sigma)
-                    #bin_3_sigma_left = h_sumcharge_proj_z.FindBin(mean - 3*sigma)
-                    #bin_3_sigma_right = h_sumcharge_proj_z.FindBin(mean + 3*sigma)
-                    #bin_4_sigma_right = h_sumcharge_proj_z.FindBin(mean + 4*sigma)
-                    #
-                    #integral_left = h_sumcharge_proj_z.Integral(bin_4_sigma_left, bin_3_sigma_left)
-                    #integral_right = h_sumcharge_proj_z.Integral(bin_3_sigma_right, bin_4_sigma_right)
-                    #
-                    #f_gauss_3_to_4 = (0.2699796-0.006334)/(2*100)
-                    #print "{0:>4} {1:>4} {2:>7.1f} {3:>6.0f} {4:>6.0f} {5:>6.1f} {6:>6.1f} {7:>6.1f}".format(run, i_roc, mean, fwhm, entries, entries * f_gauss_3_to_4, integral_left, integral_right)
 
                     print "{0:>4}   {1:>10.0f}   {2:>4}   {3:>7.1f} {4:>6.0f} {5:>6.0f} {6:>6.0f}".format(run, di_runs[run], i_iter, mean, rms, entries, rms/math.sqrt(entries))
+
+
+        if True:
+            h_cs_proj_z = h_cs.Project3D("z")
+            if i_roc in [4]:
+                
+                for i_iter in range(-1,1):
+
+                    # Ignore the zero-bin
+                    if i_iter == 0:
+                        h_cs_proj_z.SetBinContent(1,0)                    
+
+                        means["all"].append(h_cs_proj_z.GetMean())
+
+                        if run in [322, 325, 327]:
+                            means["first_three"].append(h_cs_proj_z.GetMean()) 
+                        if run in [322, 347, 348]:
+                            means["low_rate"].append(h_cs_proj_z.GetMean())
+                        if run in [333, 350, 352]:
+                            means["high_rate"].append(h_cs_proj_z.GetMean())
+
+
+                        if run in [466, 467, 469]:
+                            means["first_three"].append(h_cs_proj_z.GetMean()) 
+                        if run in [466, 475]:
+                            means["low_rate"].append(h_cs_proj_z.GetMean())
+                        if run in [470, 476, 478]:
+                            means["high_rate"].append(h_cs_proj_z.GetMean())
+                           
+                    
+                    entries = h_cs_proj_z.Integral()
+                    mean = h_cs_proj_z.GetMean()
+                    rms  = h_cs_proj_z.GetRMS()
+
+                    print "{0:>4}   {1:>10.0f}   {2:>4}   {3:>7.2f} {4:>6.0f} {5:>6.0f} {6:>6.0f}".format(run, di_runs[run], i_iter, mean, rms, entries, rms/math.sqrt(entries))
 
                 
 
     # End of loop over ROCs
 # End loop over runs
+#
 
-print means
 print "{0:>15}: {1:>10} {2:>10}".format("Runs", "Mean", "RMS")
 for k,v in means.iteritems():
 
     m = 1. * sum(v)/len(v)
     rms = math.sqrt(1.*sum([pow(x-m,2) for x in v])/len(v))
     
-    print "{0:>15}: {1:>10.1f} {2:>10.1f}".format(k, m, rms)
+    print "{0:>15}: {1:>10.3f} {2:>10.3f}".format(k, m, rms)
 
 
 ###############################
@@ -320,34 +403,38 @@ def make_plots():
                                 
             if plot_var == "cluster_size":
                 h = ROOT.TH2F("", "", 100, 1, 40000, 100, 0., 3)
-                h.GetYaxis().SetTitle("Mean cluster size")
+                h.GetYaxis().SetTitle("mean cluster size")
             elif plot_var == "cluster_size_fraction_1":
                 h = ROOT.TH2F("", "", 100, 1, 40000, 100, 0., 1.)
-                h.GetYaxis().SetTitle("Fraction of size == 1 clusters")
+                h.GetYaxis().SetTitle("fraction of size == 1 clusters")
             elif plot_var == "cluster_size_fraction_12":
                 h = ROOT.TH2F("", "", 100, 1, 40000, 100, 0., 1.)
-                h.GetYaxis().SetTitle("Fraction of 1 <= size <= 2 clusters")
+                h.GetYaxis().SetTitle("fraction of 1 <= size <= 2 clusters")
             elif plot_var == "charge":
                 h = ROOT.TH2F("", "", 100, 1, 40000, 100, 0., 30000)
-                h.GetYaxis().SetTitle("Leading charge within 4-pixel radius")
+                h.GetYaxis().SetTitle("leading charge within 4-pixel radius a.u.")
             elif plot_var == "sum_charge":
-                h = ROOT.TH2F("", "", 100, 1, 40000, 100, 0., 30000)
-                h.GetYaxis().SetTitle("Sum of charge within 4-pixel radius")
+                h = ROOT.TH2F("", "", 100, 1, 40000, 100, 0.0, 1.1)
+                h.GetYaxis().SetTitle("sum of charge within 4-pixel radius a.u.")
             elif plot_var == "second_charge":
                 h = ROOT.TH2F("", "", 100, 1, 40000, 100, 0., 15000)
-                h.GetYaxis().SetTitle("Charge of second hit in cluster")
+                h.GetYaxis().SetTitle("charge of second hit in cluster a.u.")
             else:
                 raise VariableError
 
-            h.GetXaxis().SetTitle("Flux [kHz/cm^{2}]")
+            h.GetXaxis().SetTitle("flux [kHz/cm^{2}]")
             #
 
             #h.GetXaxis().SetTitleSize(0.06)
             #h.GetYaxis().SetTitleSize(0.06)
             #h.GetXaxis().SetLabelSize(0.06)
             #h.GetYaxis().SetLabelSize(0.06)
+            
+            c.SetGrid(0,1)
 
             h.Draw()
+
+            
 
             # Prepare efficiency TGraphs
             li_grs = []
@@ -358,30 +445,46 @@ def make_plots():
                 h.GetYaxis().SetTitleOffset(1.95)
 
 
+            
 
-            for direction in ["up"]:
+            for direction in ["up","down"]:
 
                 # Choose runs to use
                 if direction == "up":
-                    li_runs = li_runs_up
+                    li_runs = li_runs_up # + li_runs_down + li_runs_final
                 elif direction == "down":
                     li_runs = li_runs_down
                 else:
                     li_runs = li_runs_final
+
 
                 # Initialize TGraph objects. For tracking we need more points for multiple event slices
                 gr = ROOT.TGraphErrors(len(li_runs))
 
                 # Loop over runs
                 for irun, run in enumerate(sorted(li_runs)):
-                    gr.SetPoint(irun, di_runs[run], di_values[run][i_roc-1])  # i_roc-1 since we have no data for ROC0
-                    gr.SetPointError(irun, 0, 490) # two sigma
+                    if plot_var == "cluster_size":
+                        gr.SetPoint(irun, di_runs[run], di_values[run][i_roc-1])  # i_roc-1 since we have no data for ROC0
+                        gr.SetPointError(irun, di_runs[run]/10., 0.06)  # i_roc-1 since we have no data for ROC0
+                    else:
+                        # charge correction
+                        #factor = 2600
+                        #factor = -27000
+                        #delta_cs = cluster_size[run][i_roc-1] - cluster_size[li_runs_up[0]][i_roc-1]
+                        #delta_charge =  factor* delta_cs
+                        #gr.SetPoint(irun, di_runs[run], (di_values[run][i_roc-1]+delta_charge)/di_values[li_runs_up[0]][i_roc-1])  # i_roc-1 since we have no data for ROC0
+                        gr.SetPoint(irun, di_runs[run], (di_values[run][i_roc-1])/di_values[li_runs_up[0]][i_roc-1])  # i_roc-1 since we have no data for ROC0
 
+                        if telescope == 1:
+                            gr.SetPointError(irun, di_runs[run]/10., 250/di_values[li_runs_up[0]][i_roc-1]) 
+                        else:
+                            gr.SetPointError(irun, di_runs[run]/10., 92/di_values[li_runs_up[0]][i_roc-1]) 
+                            
                 # Make things look nice:
                 # Legend Entries
 
                 if direction == "up":
-                    legend.AddEntry(gr, "Increasing flux", "P")
+                    legend.AddEntry(gr, "Flux", "P")
                 elif direction == "down":
                     legend.AddEntry(gr, "Decreasing flux", "P")
                 elif direction == "final":
@@ -389,7 +492,10 @@ def make_plots():
 
                 # Markers
                 # going up
-                if direction == "up":
+                if direction == "up" and plot_var == "cluster_size":
+                    gr.SetMarkerStyle(20)
+                    gr.SetMarkerColor(ROOT.kBlue)
+                elif direction == "up":
                     gr.SetMarkerStyle(22)
                     gr.SetMarkerColor(ROOT.kRed)
                 #  down
@@ -405,7 +511,7 @@ def make_plots():
                 li_grs.append(gr)
                 gr.SetMarkerSize(3)
                 gr.Draw("PSAME")
-                legend.Draw()
+                #legend.Draw()
 
                 if plot_var == "cluster_size":
                     outfile_name = "ClusterSize"
@@ -426,7 +532,17 @@ def make_plots():
 
             c.Print(outfile_name + ".png")
             c.Print(outfile_name + ".pdf")
+            
+            if plot_var == "sum_charge":
+                fun = ROOT.TF1("fun", "[0]+[1]*x")
+                gr.Fit(fun)
+                c.Print(outfile_name + "-linear.pdf")
 
+                fun2 = ROOT.TF1("fun", "[0]")
+                gr.Fit(fun2)
+                c.Print(outfile_name + "-loglinear.pdf")
+
+                    
 # End of Make Plots
 
 make_plots()
