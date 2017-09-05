37e766b956fbad2becae9eb51f0009a90660ed27
michareichmann <micha.reichmann@gmail.com>
2017-09-05 11:03:09 +0200

    test vector<vector> to write in branch

--- interface/FileWriterTracking.h
+++ interface/FileWriterTracking.h
@@ -13,12 +13,9 @@
 
 #include "PSIRootFileReader.h"
 
-using namespace std;
-
 class FileWriterTracking{
 
 private:
-
     /** constants */
     uint8_t const nRoc;
     size_t const nHits;
@@ -28,59 +25,59 @@ private:
     TFile * newfile;
     TTree * newtree;
     TMacro * names;
-    string NewFileName;
+    std::string NewFileName;
 
     int16_t TelescopeID;
 
     /** branch variables */
     uint8_t br_hit_plane_bits;
-    vector<float> * br_dia_track_pos_x;
-    vector<float> * br_dia_track_pos_y;
-    vector<float> * br_dist_to_dia;
+    std::vector<float> * br_dia_track_pos_x;
+    std::vector<float> * br_dia_track_pos_y;
+    std::vector<float> * br_dist_to_dia;
     float   br_chi2;
     float   br_chi2_x, br_chi2_y;
     float   br_angle_x, br_angle_y;
     uint8_t br_n_tracks, br_n_clusters;
-    vector<uint8_t> br_clusters_per_plane;
-    vector<uint16_t> br_n_hits;
-    vector<uint16_t> br_cluster_plane;
-    vector<uint16_t> br_cluster_col;
-    vector<uint16_t> br_cluster_row;
-    vector<float> br_cluster_xpos_tel;
-    vector<float> br_cluster_ypos_tel;
-    vector<float> br_cluster_xpos_local;
-    vector<float> br_cluster_ypos_local;
-    vector<float> br_cluster_charge;
-    vector<vector<float>* > br_charge_all;
-    vector<vector<int>* > br_cluster_size;
+    std::vector<uint8_t> br_clusters_per_plane;
+    std::vector<uint16_t> br_n_hits;
+    std::vector<uint16_t> br_cluster_plane;
+    std::vector<uint16_t> br_cluster_col;
+    std::vector<uint16_t> br_cluster_row;
+    std::vector<float> br_cluster_xpos_tel;
+    std::vector<float> br_cluster_ypos_tel;
+    std::vector<float> br_cluster_xpos_local;
+    std::vector<float> br_cluster_ypos_local;
+    std::vector<float> br_cluster_charge;
+    std::vector<std::vector<float>* > br_charge_all;
+    std::vector<std::vector<uint8_t> > * br_cluster_size;
     float br_coincidence_map;
-    vector<vector<float> *> br_track_x;
-    vector<vector<float> *> br_track_y;
-    vector<vector<float> *> br_smallest_hit_charge;
-    vector<vector<int> *> br_smallest_hit_adc;
-    vector<vector<int> *> br_smallest_hit_pos_col;
-    vector<vector<int> *> br_smallest_hit_pos_row;
-    vector<vector<float> *> br_residual_local_x;
-    vector<vector<float> *> br_residual_local_y;
-    vector<float> br_residuals_x;
-    vector<float> br_residuals_y;
+    std::vector<std::vector<float> *> br_track_x;
+    std::vector<std::vector<float> *> br_track_y;
+    std::vector<std::vector<float> *> br_smallest_hit_charge;
+    std::vector<std::vector<int> *> br_smallest_hit_adc;
+    std::vector<std::vector<int> *> br_smallest_hit_pos_col;
+    std::vector<std::vector<int> *> br_smallest_hit_pos_row;
+    std::vector<std::vector<float> *> br_residual_local_x;
+    std::vector<std::vector<float> *> br_residual_local_y;
+    std::vector<float> br_residuals_x;
+    std::vector<float> br_residuals_y;
 
     /** some functions*/
-    string getFileName(string);
+    std::string getFileName(std::string &);
 
 public:
 
     /** ============================
      CONSTRUCTOR
      =================================*/
-    FileWriterTracking(string, uint8_t, PSIFileReader * FR);
+    FileWriterTracking(std::string, uint8_t, PSIFileReader * FR);
     ~FileWriterTracking();
 
 
     /** ============================
      GET-FUNCTIONS
      =================================*/
-    string FileName() {return NewFileName; }
+    std::string FileName() {return NewFileName; }
     TTree * InTree() { return intree; }
     uint8_t HitPlaneBits() { return br_hit_plane_bits; }
     uint8_t nTracks() { return br_n_tracks; }
@@ -92,22 +89,22 @@ public:
     float   Chi2() { return br_chi2; }
     float   Chi2X() { return br_chi2_x; }
     float   Chi2Y() { return br_chi2_y; }
-    vector<vector<float>* > ChargeAll() { return br_charge_all; }
-    vector<vector<int>* > ClusterSize() { return br_cluster_size; }
+    std::vector<std::vector<float>* > ChargeAll() { return br_charge_all; }
+    std::vector<std::vector<uint8_t> > * ClusterSize() { return br_cluster_size; }
     size_t GetNHits() { return nHits; }
     float   GetCoincidenceMap() { return br_coincidence_map; }
-    vector<uint16_t> ClusterRow() { return br_cluster_row; }
-    vector<uint16_t> ClusterCol() { return br_cluster_col; }
-    vector<vector<float>* > TrackX() { return br_track_x; }
-    vector<vector<float>* > TrackY() { return br_track_y; }
-    vector<vector<float>* > SmallestHitCharge() { return br_smallest_hit_charge; }
-    vector<vector<int>* > SmallestHitADC() { return br_smallest_hit_adc; }
-    vector<vector<int>* > SmallestHitPosCol() { return br_smallest_hit_pos_col; }
-    vector<vector<int>* > SmallestHitPosRow() { return br_smallest_hit_pos_row; }
-    vector<vector<float>* > ResidualLocalX() { return br_residual_local_x; }
-    vector<vector<float>* > ResidualLocalY() { return br_residual_local_y; }
-    vector<float> ResidualX() { return br_residuals_x; }
-    vector<float> ResidualY() { return br_residuals_y; }
+    std::vector<uint16_t> ClusterRow() { return br_cluster_row; }
+    std::vector<uint16_t> ClusterCol() { return br_cluster_col; }
+    std::vector<std::vector<float>* > TrackX() { return br_track_x; }
+    std::vector<std::vector<float>* > TrackY() { return br_track_y; }
+    std::vector<std::vector<float>* > SmallestHitCharge() { return br_smallest_hit_charge; }
+    std::vector<std::vector<int>* > SmallestHitADC() { return br_smallest_hit_adc; }
+    std::vector<std::vector<int>* > SmallestHitPosCol() { return br_smallest_hit_pos_col; }
+    std::vector<std::vector<int>* > SmallestHitPosRow() { return br_smallest_hit_pos_row; }
+    std::vector<std::vector<float>* > ResidualLocalX() { return br_residual_local_x; }
+    std::vector<std::vector<float>* > ResidualLocalY() { return br_residual_local_y; }
+    std::vector<float> ResidualX() { return br_residuals_x; }
+    std::vector<float> ResidualY() { return br_residuals_y; }
 
     /** ============================
         SETTER METHODS
@@ -123,7 +120,7 @@ public:
     void setChi2X(float value) { br_chi2_x = value; }
     void setChi2Y(float value) { br_chi2_y = value; }
     void setChargeAll(uint8_t iRoc, float value) { br_charge_all[iRoc]->push_back(value); }
-    void setClusterSize(uint8_t iRoc, int value) { br_cluster_size[iRoc]->push_back(value); }
+    void setClusterSize(uint8_t iRoc, int value) { br_cluster_size->at(iRoc).push_back(value); }
     void setClusters(uint8_t iRoc, uint8_t value) { br_clusters_per_plane[iRoc] = value; }
     void setClusterPlane(uint16_t value) { br_cluster_plane.push_back(value); }
     void setNHits(uint8_t iRoc, uint16_t value) { br_n_hits[iRoc] = value; }
