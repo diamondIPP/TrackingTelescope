3d55ec50e5f3a960728205d4d1b266928d593eaa
michareichmann <micha.reichmann@gmail.com>
2017-09-06 14:30:39 +0200

    adding progressbar

--- interface/Utils.h
+++ interface/Utils.h
@@ -7,12 +7,35 @@
 
 #include <string>
 #include <vector>
+#include <sys/ioctl.h>
+
+struct winsize;
 
 namespace tel{
 
-    std::string trim(const std::string &, std::string="\t\n\r\v ");
-    std::vector<std::string> split(const std::string &, const char & = '\t');
-    double distance(std::pair<float, float>, std::pair<float, float>);
+  std::string trim(const std::string &, std::string="\t\n\r\v ");
+  std::vector<std::string> split(const std::string &, const char & = '\t');
+  double distance(std::pair<float, float>, std::pair<float, float>);
+
+  class ProgressBar {
+  private:
+    uint32_t nEvents;
+    uint32_t currentEvent;
+    bool useETA;
+    struct winsize w;
+    uint8_t barLength;
+    uint16_t updateFrequency;
+    clock_t lastTime;
+    uint8_t nCycles;
+    float timePerCycle;
+
+  public:
+    ProgressBar(uint32_t, bool use_ETA=true, uint16_t update=100);
+    ~ProgressBar() = default;
+    void update(uint32_t=0);
+    void averageTime();
+    float getTime();
+  };
 }
 
 #endif //TRACKINGTELESCOPE_UTILS_H
--- src/Utils.cc
+++ src/Utils.cc
@@ -4,6 +4,8 @@
 
 #include "Utils.h"
 #include <sstream>
+#include <iostream>
+#include <iomanip>
 #include <cmath>
 
 using namespace std;
@@ -32,5 +34,53 @@ namespace tel {
   double distance(std::pair<float, float> p1, std::pair<float, float> p2){
 
     return sqrt(pow(p1.first - p2.first, 2) + pow(p1.second - p2.second, 2));
+  }
+
+  ProgressBar::ProgressBar(uint32_t n_events, bool use_ETA, uint16_t update): nEvents(n_events), currentEvent(0), useETA(use_ETA), updateFrequency(update), lastTime(clock()),
+                                                                              nCycles(0), timePerCycle(0) {
+    ioctl(0, TIOCGWINSZ, &w);
+    uint8_t diff = not use_ETA ? uint8_t(26) : uint8_t(37);
+    barLength = uint8_t(w.ws_col - diff);
+  }
+
+  void ProgressBar::update(uint32_t event) {
+
+    if (currentEvent == 0u)
+      cout << endl;
+
+    currentEvent = (event == 0u ? currentEvent + 1 : event);
+
+    if (currentEvent % updateFrequency == 0 && (currentEvent != 0u)){
+      stringstream ss;
+      ss << "\rEvent: "  << setw(7) << setfill('0') << fixed << currentEvent;
+      auto current_bar_length = uint8_t(float(currentEvent) / nEvents * barLength);
+      ss << " [" << string(current_bar_length, '=') << ">" << string(barLength - current_bar_length, ' ') << "] ";
+      ss << setprecision(2) << setw(6) << setfill('0') << fixed << float(currentEvent) / nEvents * 100 << "%";
+      if (useETA) {
+        averageTime();
+        float tot = (nEvents - currentEvent) / updateFrequency * timePerCycle;
+        ss << setprecision(0) << " ETA: " << setw(2) << setfill('0') << tot / 60 << ":" << setw(2) << setfill('0') << tot - int(tot) / 60 * 60;
+      }
+      cout << ss.str() << flush;
+    }
+    if (currentEvent == nEvents) {
+      cout << "\rEvent: "  << setw(7) << setfill('0') << fixed << nEvents;
+      cout << " [" << string(barLength, '=') << ">" << "] 100.00%" << endl;
+    }
+  }
+
+  float ProgressBar::getTime() {
+    clock_t now = clock();
+    float ret = float(now - lastTime) / CLOCKS_PER_SEC;
+    lastTime = now;
+    return ret;
+  }
+
+  void ProgressBar::averageTime() {
+    if (nCycles < 10)
+      timePerCycle = (timePerCycle * nCycles + getTime()) / (nCycles + 1);
+    else
+      timePerCycle = float(.98) * timePerCycle + float(.02) * getTime();
+    nCycles++;
   }
 }
