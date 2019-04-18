875145b8ac73cfd0624ba5050cfdc587937b923b
michareichmann <micha.reichmann@gmail.com>
2019-04-18 11:53:23 +0200

    add EventLoop(), and plane fields to simplify the code

--- interface/DoAlignment.h
+++ interface/DoAlignment.h
@@ -22,11 +22,12 @@ public:
   void InitHistograms();
   void ResetHistograms();
   void SaveHistograms(unsigned, int ind=-1);
-  void SaveGraphs(unsigned);
   int Align();
   void PrintAlignment();
   std::vector<unsigned short> GetOrderedPlanes();
   static void FormatHistogram(TH1 *, const std::string&, float, const std::string&, float);
+  std::pair<float, float> GetMeanResiduals(unsigned short i_plane);
+  std::pair<float, float> GetRMS(unsigned short i_plane);
 
 private:
   std::string InFileName;
@@ -38,11 +39,14 @@ private:
   float const TotResThreshold;
   PSIFileReader * FR;
   PSIFileReader * InitFileReader();
+  void EventLoop(const std::vector<unsigned short>&);
   unsigned MaxEventNumber;
   tel::ProgressBar * ProgressBar;
   float Now;
   unsigned short const MaximumSteps;
   std::vector<unsigned short> OrderedPlanes;
+  std::vector<unsigned short> InnerPlanes;
+  std::vector<unsigned short> PlanesToAlign;
   /** Histograms
       hResidual:    x=dX / y=dY
       hResidualXdY: x=X  / y=dY
@@ -50,8 +54,6 @@ private:
   std::vector<TH2F> hResidual;
   std::vector<TH2F> hResidualXdY;
   std::vector<TH2F> hResidualYdX;
-  std::vector<TGraph> gResidualXdY;
-  std::vector<TGraph> gResidualYdX;
 };
 
 #endif // DoAlignment_h
--- src/DoAlignment.cc
+++ src/DoAlignment.cc
@@ -33,6 +33,8 @@ Alignment::Alignment(string in_file_name, const TString & run_number, short tele
   MaxEventNumber = unsigned(dynamic_cast<PSIRootFileReader*>(FR)->fTree->GetEntries());
 //  MaxEventNumber = 20000;
   OrderedPlanes = GetOrderedPlanes();
+  InnerPlanes = vector<unsigned short>(OrderedPlanes.begin() + 1, OrderedPlanes.end() - 1);
+  PlanesToAlign = AlignOnlyInnerPlanes ? InnerPlanes : vector<unsigned short>(OrderedPlanes.begin() + 1, OrderedPlanes.end());
   FR->GetAlignment()->ResetPlane(1, OrderedPlanes.at(0)); /** the first plane in z should be kept fixed */
   ProgressBar = new tel::ProgressBar(MaxEventNumber - 1);
   /** Apply Masking */
@@ -62,55 +64,111 @@ PSIFileReader * Alignment::InitFileReader() {
   return tmp;
 }
 
-void Alignment::PreAlign() {
+void Alignment::EventLoop(const std::vector<unsigned short> & planes) {
 
-  /** coarsely move the two inner planes to minimise the residuals without rotations */
-  cout << "\n***************************\nPART ONE - COARSE ALIGNMENT\n***************************\n" << endl;
-  FR->ResetFile();
-  FR->SetPlanesUnderTest(1, 2); /** ignore inner planes for tracking */
-  ResetHistograms(); /** Reset residual histograms */
-  Now = clock();
-  PrintAlignment();
-        
-  /** EVENT LOOP */
   for (uint32_t i_event = 0; FR->GetNextEvent() >= 0; ++i_event) {
-
     if (i_event >= MaxEventNumber) break;
     ProgressBar->update(i_event); /** print progress */
 
     if (FR->NTracks() != 1) continue; /** proceed only if we have exactly one track */
     PLTTrack * Track = FR->Track(0);
-    for (unsigned i_plane(1); i_plane < NPlanes - 1; ++i_plane) {
+
+    for (auto i_plane: planes) {
 
       PLTPlane * Plane = FR->Plane(i_plane);
       if (Plane->NClusters() != 1) continue; /** proceed only if there is exactly one cluster */
 
       PLTCluster * Cluster = Plane->Cluster(0);
-      pair<float, float> l_res = Track->GetResiduals(*Cluster, *FR->GetAlignment());
-//      cout << "Track Residuals for " << i_plane << " (x/y): " << l_res.first << "/" << l_res.second << endl;
-      if (fabs(l_res.first) >= 2 or fabs(l_res.second) >= 2) continue; /** only proceed if the residual is smaller than 2mm in x or y */
+      /** if the plane is under test, calculate the residuals otherwise just take them from the track */
+      pair<float, float> dR = (FR->IsPlaneUnderTest(i_plane)) ? Track->GetResiduals(*Cluster, *FR->GetAlignment()) : Track->GetResiduals(i_plane);
+      if (fabs(dR.first) >= 2 or fabs(dR.second) >= 2) continue; /** only proceed if the residual is smaller than 2mm in x or y */
 
-      hResidual[i_plane].Fill(l_res.first, l_res.second); // dX vs dY
-      hResidualXdY[i_plane].Fill(Cluster->LX(), l_res.second); // X vs dY
-      hResidualYdX[i_plane].Fill(Cluster->LY(), l_res.first); // Y vs dX
+      hResidual[i_plane].Fill(dR.first, dR.second); // dX vs dY
+      hResidualXdY[i_plane].Fill(Cluster->LX(), dR.second); // X vs dY
+      hResidualYdX[i_plane].Fill(Cluster->LY(), dR.first); // Y vs dX
     }
-  } // end event loop
+  }
+} // end EventLoop
+
+void Alignment::PreAlign() {
+
+  /** coarsely move the two inner planes to minimise the residuals without rotations */
+  cout << "\n***************************\nPART ONE - COARSE ALIGNMENT\n***************************\n" << endl;
+  FR->ResetFile();
+  FR->SetPlanesUnderTest(InnerPlanes); /** ignore inner planes for tracking */
+  ResetHistograms(); /** Reset residual histograms */
+  PrintAlignment();
+
+  Now = clock();
+  EventLoop(InnerPlanes); /** Loop over all events and fill histograms */
   cout << "\nLoop duration:" << (clock() - Now) / CLOCKS_PER_SEC << endl;
+  cout << "RES " << hResidual.at(1).GetMean() << endl;
 
-  for (unsigned i_plane(1); i_plane < NPlanes - 1; i_plane++){
-
-    double x_res = hResidual[i_plane].GetMean(1), x_rms = hResidual[i_plane].GetRMS(1);
-    double y_res = hResidual[i_plane].GetMean(2), y_rms = hResidual[i_plane].GetRMS(2);
-    cout << "\nPLANE " << i_plane << "\n--RESIDUALS: " << setprecision(3) << x_res << " (" << x_rms << "), " << y_res << " (" << y_rms << ")" << endl;
+  for (auto i_plane: InnerPlanes){
+    pair<float, float> dR = GetMeanResiduals(i_plane);
+    pair<float, float> rms = GetRMS(i_plane);
+    cout << "\nPLANE " << i_plane << "\n--RESIDUALS: " << setprecision(3) << dR.first << " (" << rms.first << "), " << dR.second << " (" << rms.second << ")" << endl;
 
     /** update the alignment */
     cout << "Before: " << FR->GetAlignment()->LX(1, i_plane) << " / " << FR->GetAlignment()->LY(1, i_plane) << endl;
-    FR->GetAlignment()->AddToLX(1, i_plane, float(x_res));
-    FR->GetAlignment()->AddToLY(1, i_plane, float(y_res));
+    FR->GetAlignment()->AddToLX(1, i_plane, float(dR.first));
+    FR->GetAlignment()->AddToLY(1, i_plane, float(dR.second));
     cout << "After:  " << FR->GetAlignment()->LX(1, i_plane) << " / " << FR->GetAlignment()->LY(1, i_plane) << endl;
 
     SaveHistograms(i_plane);
   }
+}
+
+int Alignment::Align() {
+
+  cout << "\n*************************\nPART TWO - FINE ALIGNMENT\n*************************\n" << endl;
+  for (int i_align(0); i_align < MaximumSteps; i_align++) {
+    cout << "BEGIN ITERATION " << i_align + 1 << " OUT OF " << MaximumSteps << endl;
+    FR->ResetFile();
+    FR->SetAllPlanes();
+    ProgressBar->reset();
+
+    EventLoop(PlanesToAlign); /** Loop over all events and fill histograms */
+
+    float total_angle = 0;
+    float total_res = 0;
+
+    for (auto i_plane: PlanesToAlign) {
+
+      double x_res = hResidual[i_plane].GetMean(1), x_rms = hResidual[i_plane].GetRMS(1);
+      double y_res = hResidual[i_plane].GetMean(2), y_rms = hResidual[i_plane].GetRMS(2);
+      cout << "\nPLANE " << i_plane << "\n--RESIDUALS: " << setprecision(3) << x_res << " (" << x_rms << "), " << y_res << " (" << y_rms << ")" << endl;
+
+      double angle = atan(hResidualXdY[i_plane].GetCorrelationFactor());
+      TF1 fX = TF1("fX", "pol1"), fY = TF1("fY", "pol1");
+      hResidualXdY[i_plane].Fit(&fX, "q");
+      hResidualYdX[i_plane].Fit(&fY, "q");
+
+      double angle_x = atan(fX.GetParameter(1)), angle_y = atan(fY.GetParameter(1));
+      total_angle += fabs(angle_x);
+      total_res += fabs(x_res) + fabs(y_res);
+      cout << "--Angle Corr: " << angle << ", Angle XdY: " << angle_x << ", Angle YdX: " << angle_y << endl;
+
+      SaveHistograms(i_plane, (total_angle < AngleThreshold && total_res < TotResThreshold) ? -1 : i_align);
+
+      FR->GetAlignment()->AddToLR(1, i_plane, float(angle_x)); // DA: this was ... other_angle/3.
+      FR->GetAlignment()->AddToLX(1, i_plane, float(x_res));
+      FR->GetAlignment()->AddToLY(1, i_plane, float(y_res));
+    }
+    cout << "END ITERATION " << i_align + 1 << " OUT OF 14" << endl;
+    cout << "\nSum of angle corrections:    " << total_angle << endl;
+    cout << "Sum of residuals in x and y: " << total_res << endl;
+
+    if (total_angle < AngleThreshold && total_res < TotResThreshold){
+      std::cout << "Total angle is below " << AngleThreshold << " and Total Residual is below " << TotResThreshold << "=> stopping alignment." << endl;
+      break;
+    }
+    cout << endl;
+  } // end alignment loop
+
+  cout << "Saving alignment file \"" << OutFileName << "\" with the following parameters:" << endl;
+  PrintAlignment();
+  return 0;
 }
 
 void Alignment::InitHistograms() {
@@ -119,8 +177,6 @@ void Alignment::InitHistograms() {
     hResidual.emplace_back(        Form("Residual_ROC%i", i_plane),    Form("Residual_ROC%i",    i_plane), 400, -.5, .5, 400, -.5, .5);
     hResidualXdY.emplace_back(TH2F(Form("ResidualXdY_ROC%i", i_plane), Form("ResidualXdY_ROC%i", i_plane), 133, -1, 0.995, 400, -.5, .5));
     hResidualYdX.emplace_back(TH2F(Form("ResidualYdX_ROC%i", i_plane), Form("ResidualYdX_ROC%i", i_plane), 201, -1, 1, 400, -.5, .5));
-    gResidualXdY.emplace_back(TGraph());
-    gResidualYdX.emplace_back(TGraph());
   }
 }
 
@@ -164,98 +220,6 @@ void Alignment::SaveHistograms(unsigned i_plane, int ind) {
   Can.SaveAs(OutDir + "/" + sub_dir + TString(hResidualYdX[i_plane].GetName()) + FileType);
 }
 
-void Alignment::SaveGraphs(unsigned i_plane) {
-
-  TCanvas c;
-  c.cd();
-  gResidualXdY[i_plane].Draw("AP*");
-  c.SaveAs(OutDir + "/" + TString::Format("gRes%i", i_plane) + ".png");
-}
-
-int Alignment::Align() {
-
-  cout << "\n*************************\nPART TWO - FINE ALIGNMENT\n*************************\n" << endl;
-  for (int i_align(0); i_align < MaximumSteps; i_align++) {
-    cout << "BEGIN ITERATION " << i_align + 1 << " OUT OF " << MaximumSteps << endl;
-    FR->ResetFile();
-    FR->SetAllPlanes();
-    ProgressBar->reset();
-
-    /** EVENT LOOP */
-    for (uint32_t i_event = 0; FR->GetNextEvent() >= 0; ++i_event) {
-      if (i_event >= MaxEventNumber) break;
-      ProgressBar->update(i_event); /** print progress */
-
-      if (FR->NTracks() != 1) continue; /** proceed only if we have exactly one track */
-      PLTTrack * Track = FR->Track(0);
-
-      for (unsigned i_plane(0); i_plane < NPlanes; i_plane++){
-
-        float d_LX = Track->LResidualX(i_plane); // local distance between track and cluster position.
-        float d_LY = Track->LResidualY(i_plane);
-
-        PLTCluster * Cluster = FR->Plane(i_plane)->Cluster(0);
-        float cl_LX = Cluster->LX();
-        float cl_LY = Cluster->LY();
-
-        hResidual[i_plane].Fill(d_LX, d_LY);
-        // DA: take into account only events whose local residuals are less than 1000mm in x and y
-        if (fabs(d_LX) > 2 or fabs(d_LY) > 2) continue; /** only proceed if both residual are smaller than 2cm */
-          hResidualXdY[i_plane].Fill(cl_LX, d_LY); //fill with cluster local position vs residual
-          hResidualYdX[i_plane].Fill(cl_LY, d_LX);
-          gResidualXdY[i_plane].SetPoint(gResidualXdY[i_plane].GetN(), cl_LX, d_LY);
-          gResidualYdX[i_plane].SetPoint(gResidualYdX[i_plane].GetN(), cl_LY, d_LX);
-      }
-    } // end event loop
-
-    float total_angle = 0;
-    float total_res = 0;
-
-    vector<unsigned short> planes = OrderedPlanes;
-    planes.erase(planes.begin()); /** keep the first plane unchanged */
-    for (auto i_plane: planes) {
-
-      double x_res = hResidual[i_plane].GetMean(1), x_rms = hResidual[i_plane].GetRMS(1);
-      double y_res = hResidual[i_plane].GetMean(2), y_rms = hResidual[i_plane].GetRMS(2);
-      cout << "\nPLANE " << i_plane << "\n--RESIDUALS: " << setprecision(3) << x_res << " (" << x_rms << "), " << y_res << " (" << y_rms << ")" << endl;
-
-
-      double angle = atan(hResidualXdY[i_plane].GetCorrelationFactor());
-      TF1 fX = TF1("fX", "pol1"), fY = TF1("fY", "pol1");
-//            gResidualXdY[i_plane].Fit(&linear_fun);
-//            gResidualYdX[i_plane].Fit(&linear_fun2);
-      hResidualXdY[i_plane].Fit(&fX, "q");
-      hResidualYdX[i_plane].Fit(&fY, "q");
-
-      double angle_x = atan(fX.GetParameter(1)), angle_y = atan(fY.GetParameter(1));
-      total_angle += fabs(angle_x);
-      total_res += fabs(x_res) + fabs(y_res);
-      cout << "--Angle Corr: " << angle << ", Angle XdY: " << angle_x << ", Angle YdX: " << angle_y << endl;
-
-      SaveGraphs(i_plane);
-      SaveHistograms(i_plane, (total_angle < AngleThreshold && total_res < TotResThreshold) ? -1 : i_align);
-
-      FR->GetAlignment()->AddToLR(1, i_plane, float(angle_x)); // DA: this was ... other_angle/3.
-      if (AlignOnlyInnerPlanes and i_plane == planes.back()) continue; /** only align the middle planes */
-      FR->GetAlignment()->AddToLX(1, i_plane, float(x_res));
-      FR->GetAlignment()->AddToLY(1, i_plane, float(y_res));
-    }
-    cout << "END ITERATION " << i_align + 1 << " OUT OF 14" << endl;
-    cout << "\nSum of angle corrections:    " << total_angle << endl;
-    cout << "Sum of residuals in x and y: " << total_res << endl;
-
-    if (total_angle < AngleThreshold && total_res < TotResThreshold){
-      std::cout << "Total angle is below " << AngleThreshold << " and Total Residual is below " << TotResThreshold << "=> stopping alignment." << endl;
-      break;
-    }
-    cout << endl;
-  } // end alignment loop
-
-  cout << "Saving alignment file \"" << OutFileName << "\" with the following parameters:" << endl;
-  PrintAlignment();
-  return 0;
-}
-
 Alignment::~Alignment() {
 
   delete FR;
@@ -294,3 +258,6 @@ void Alignment::FormatHistogram(TH1 * h, const string & x_tit, float x_off, cons
   y_axis->SetTitle(y_tit.c_str());
   y_axis->SetTitleOffset(y_off);
 }
+
+std::pair<float, float> Alignment::GetMeanResiduals(unsigned short i_plane) { return std::make_pair(hResidual[i_plane].GetMean(1), hResidual[i_plane].GetMean(2)); }
+std::pair<float, float> Alignment::GetRMS(unsigned short i_plane) { return std::make_pair(hResidual[i_plane].GetRMS(1), hResidual[i_plane].GetRMS(2)); }
