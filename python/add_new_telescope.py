7975f9b7ffe40aca5bea31255046a4a07c9e556d
michareichmann <micha.reichmann@gmail.com>
2021-02-27 13:14:38 +0100

    add python script to add a new tel

---
+++ python/add_new_telescope.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# small script to add new telescope
+# makes an entry to the data/config.txt
+
+from argparse import ArgumentParser
+from ROOT import TFile
+from numpy import frombuffer
+from os.path import dirname, realpath, join
+
+
+def get_n_rocs(filename):
+    f = TFile(filename)
+    t = f.Get('tree')
+    n = t.Draw('plane', '', 'goff')
+    return int(max(frombuffer(t.GetVal(0), count=n)) + 1)
+
+
+def get_last_tel(filename):
+    with open(filename) as f:
+        return max([int(line.split()[0]) for line in f.readlines() if not line.startswith('#')])
+
+
+def get_z_pos(year):
+    if year < 2016:
+        return 0
+    elif year == 2016:
+        return 1
+    elif year > 2016:
+        return 2
+
+
+def save_config(filename, n_rocs):
+    last_tel = get_last_tel(filename)
+    new_tel = input('Enter the telescope number, press enter for default ({}): '.format(last_tel + 1))
+    tel = last_tel + 1 if not new_tel else int(new_tel)
+    mask = input('Enter the outer mask file number, press enter for default: ')
+    mask = 0 if not mask else int(mask)
+    cal = input('Enter the calibration number (press enter for default ({})): '.format(tel))
+    cal = tel if not cal else int(cal)
+    year = int(input('Enter the year (YYYY): '))
+    z_pos = get_z_pos(year)
+    cmt = input('Enter a comment (Month, DUT, ...): ')
+    with open(filename, 'a') as f:
+        f.write('\n{: 3d}{: 4d}{: 8d}{: 5d}{: 6d}{: 6d} # {}'.format(tel, n_rocs, mask, cal, z_pos, year, cmt))
+
+
+if __name__ == '__main__':
+
+    d = dirname(dirname(realpath(__file__)))
+
+    p = ArgumentParser()
+    p.add_argument('run')
+    args = p.parse_args()
+
+    config = join(d, 'config', 'main.txt')
+    save_config(config, get_n_rocs(args.run))
